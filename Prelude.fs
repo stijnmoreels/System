5258c26b7709415872044490c6a2dbad86323572
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-03-20 08:30:33 +0100

    Update Prelude.fs

--- Prelude.fs
+++ Prelude.fs
@@ -5,6 +5,6 @@ module Prelude =
 
     /// Clean up resources associated with the input object after the completion of the given function. 
     /// Cleanup occurs even when an exception is raised by the protected code.
-    let inline using (x : ^a when ^a : (member Dispose : unit -> unit)) f =
+    let inline using f (x : ^a when ^a : (member Dispose : unit -> unit)) =
         try f x
         finally (^a : (member Dispose : unit -> unit) x)
