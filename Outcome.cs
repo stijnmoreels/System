b57acb6b3a756e9f136c277b04cb0a93dc587866
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2019-12-31 12:25:41 +0100

    Update Outcome.cs

--- Outcome.cs
+++ Outcome.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace FPrimitive.CSharp.Tests
+namespace System
 {
     /// <summary>
     /// Represents an abstracted result that can either represent a successful or failure outcome.
