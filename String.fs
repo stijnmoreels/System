9d2699569d3575f7796ba2a3363824e34c679e0d
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-09-03 08:56:50 +0200

    Create String.fs

---
+++ String.fs
@@ -0,0 +1,10 @@
+namespace System
+
+/// Common extensions on the System.String type.
+module String =
+  /// Determines if a string is not `null`.
+  let isNotNull (x : string) = isNull x |> not
+  /// Determines if a string is not empty.
+  let isNotEmpty (x : string) = String.IsNullOrEmpty x |> not
+  /// Determines if a string is not blank.
+  let isNotBlank (x : string) = String.IsNullOrWhiteSpace x |> not
